805023f3b6445c212ec80bcbc96f37aab3e674c1
PvttJebus <149273873+PvttJebus@users.noreply.github.com>
2024-03-12 12:24:55 -0400

    Opened a project

---
+++ Team 4 Platform Project - Assignment 4/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Team_4_Platform_Project___Assignment_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
